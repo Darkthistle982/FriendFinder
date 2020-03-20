fd0364d1c7dcc6bea207dd07cdee93df908c5763
Brian Allen <brianallen2401@gmail.com>
2020-03-20 13:28:47 -0600

    finished and connected/tested routing to html. Working

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -2,12 +2,14 @@ const path = require("path");
 
 module.exports = function(app) {
   app.get("/", function(request, response) {
-    const viewHomePath = path.join(__dirname, "public", "home.html");
+    const viewHomePath = path.join(__dirname, "..", "public", "home.html");
+    console.log(viewHomePath);
     response.sendFile(viewHomePath);
+
   });
 
   app.get("/survey", function(request, response) {
-    const viewSurveyPath = path.join(__dirname, "public", "survey.html");
+    const viewSurveyPath = path.join(__dirname, "..", "public", "survey.html");
     response.sendFile(viewSurveyPath);
   });
 };
