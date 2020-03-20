90bbc19d271af608aa290434905a6ba8d941f088
Brian Allen <brianallen2401@gmail.com>
2020-03-20 11:26:57 -0600

    added require to route js docs. added listener

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const path = require('path');
-
+const apiRoutes = require('./app/routing/apiRoutes.js');
+const htmlRoutes = require('./app/routing/htmlRoutes.js');
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+});
