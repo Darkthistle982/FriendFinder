bcee9bd83a53763f30175d771c9ea0d86ff4bf0a
Brian Allen <brianallen2401@gmail.com>
2020-03-20 14:56:41 -0600

    wrote get statement in api/freinds for pulling the json data from teh friends.js json object.

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,2 +1,8 @@
-const path = require('path');
+const path = require("path");
+const friends = require('../data/friends');
 
+module.exports = function(app) {
+  app.get("/api/friends", function(request, response) {
+      return response.json(friends);
+  });
+};
