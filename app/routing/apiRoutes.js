0e4dc8bf22e3fb7e31240f36646892ab2175cf44
Brian Allen <brianallen2401@gmail.com>
2020-03-20 16:33:16 -0600

    adjusted basic apiRoutes code for the compare logic. Felt lost so I moved on to another part

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -5,4 +5,16 @@ module.exports = function(app) {
   app.get("/api/friends", function(request, response) {
       return response.json(friends);
   });
+
+  app.post("/api/friends", function(request, response) {
+      let newFriend = request.body;
+      newFriend.routeName = newFriend.name.replace(/\s+/g, "").toLowerCase();
+      friends.push(newFriend);
+      response.json(newFriend);
+  });
+
+
+
+
+
 };
