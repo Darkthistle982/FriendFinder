f8b259ea0a658e554dece22e9062c63ba245803c
Brian Allen <brianallen2401@gmail.com>
2020-03-20 13:33:03 -0600

    uncommented line to activate connection to other js files

--- server.js
+++ server.js
@@ -2,13 +2,15 @@ const express = require('express');
 const path = require('path');
 const apiRoutes = require('./app/routing/apiRoutes.js');
 const htmlRoutes = require('./app/routing/htmlRoutes.js');
-const app = express();
+
 const PORT = process.env.PORT || 8080;
+const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-
+htmlRoutes(app);
+// apiRoutes(app);
 
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
