1b8ac0cb12098bacbe019f80a090ad9ea1a2dfcb
Brian Allen <brianallen2401@gmail.com>
2020-03-20 15:21:24 -0600

    created friends object and export

---
+++ app/data/friends.js
@@ -0,0 +1,39 @@
+let friends = [
+    {
+        name: "Beauregard",
+        photo: "../images/Beauregard.jpg",
+        scores: [2, 5, 1, 5, 4, 4, 3, 5, 2, 5]
+    },
+    {
+        name: "Caduceus",
+        photo: "../images/Caduceus_portrait.jpg",
+        scores: [1, 1, 5, 2, 2, 2, 5, 3, 5, 1]
+    },
+    {
+        name: "Caleb Widogast",
+        photo: "../images/Caleb_Widogast.jpg",
+        scores: [5, 2, 2, 5, 1, 1, 3, 5, 4, 4]
+    },
+    {
+        name: "Fjord",
+        photo: "../images/Fjord.jpg",
+        scores: [2, 4, 2, 1, 5, 2, 5, 3, 4, 5]
+    },
+    {
+        name: "Jester",
+        photo: "../images/Jester.jpg",
+        scores: [1, 1, 3, 1, 4, 5, 2, 2, 5, 2]
+    },
+    {
+        name: "Nott the Brave",
+        photo: "../images/Nott_the_Brave.jpg",
+        scores: [4, 5, 5, 4, 5, 5, 1, 5, 2, 1]
+    },
+    {
+        name: "Yasha",
+        photo: "../images/Yasha.jpg",
+        scores: [2, 4, 3, 2, 2, 3, 5, 5, 5, 1]
+    }
+]
+
+module.exports = friends;
